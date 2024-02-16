51cb84e035abb1ffaafa32956927da6a0a945793
soriv <sorivma03.06@gmail.com>
2024-02-16 22:38:45 +0300

    add task_3

---
+++ js_labs/lab_1/task_3.js
@@ -0,0 +1,23 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+divisors = [2,3,6,8,10]
+
+function findDivisors(number) {
+    for (let i = divisors.length - 1; i >= 0; i --) {
+        if (number % divisors[i] == 0) {
+            console.log(`Число делится на ${divisors[i]}`)
+            return
+        }
+    }
+    console.log("Не делится")
+}
+
+rl.question('', (number) => {
+    findDivisors(parseInt(number))
+    rl.close();
+})
